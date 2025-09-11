78473d1095c2eb5be3fb7df18b2ff6d0649923db
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-09-11 14:48:37 +1000

    update

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -16,41 +16,15 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto gap-4">
-            {/* {user && user._id ? (
-              <>
-                <Nav.Link as={Link} to="/dashboard">
-                  Dashboard
-                </Nav.Link>
-
-                <Nav.Link as={Link} to="/books/pub-books">
-                  Books
-                </Nav.Link>
-                <Button
-                  onClick={() => {
-                    // remove user data onclick
-                    dispatch(logoutAction());
-                  }}
-                >
-                  Logout
-                </Button>
-              </>
-            ) : ( */}
-            {/* )} */}
-            <>
-              {/* <Nav.Link as={Link} to="/books/pub-books">
-                Books
-              </Nav.Link> */}
-
-              <Nav.Link as={Link} to="/dashboard">
-                Dashboard
-              </Nav.Link>
-              <Nav.Link as={Link} to="/login">
-                Login
-              </Nav.Link>
-              <Nav.Link as={Link} to="/signup">
-                Signup
-              </Nav.Link>
-            </>
+            <Nav.Link as={Link} to="/dashboard">
+              Dashboard
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="/signup">
+              Signup
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
