98aff2ed866a0677d91bf031fe48aac9d380cec8
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-09-11 12:21:18 +1000

    customer fe initialised

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center d-flex align-items-center justify-content-center bg-dark text-white vh-100">
+        Welcome to the Customer Site
+      </h1>
+    </>
+  );
+}
+
+export default App;
