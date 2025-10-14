30c3e6c9db3dfa8ebbc1b8064402e6f41abe9843
Manish <100024795+manishhh08@users.noreply.github.com>
2025-10-14 14:18:36 +1100

    Update productAction.js

--- src/features/product/productAction.js
+++ src/features/product/productAction.js
@@ -1,10 +1,10 @@
-import { fetchActiveProducts, fetchAllProducts } from "./productAPI";
+import { fetchAllProducts } from "./productAPI";
 import { setProducts } from "./productSlice";
 
 
 export const fetchAllProductsAction = () => async (dispatch) => {
     try {
-        const response = await fetchActiveProducts();
+        const response = await fetchAllProducts();
         console.log("Fetched products:", response);
         if (response.status === "success") {
             dispatch(setProducts(response.products));
