c3c335b01abb593eb6c93da89bf4e8e92e3e700d
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-10-27 14:09:31 +1100

    link removed from main category

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -43,12 +43,9 @@ const SideBar = ({ categories, subcategories, isOpen, closeSidebar }) => {
               style={{ cursor: "pointer" }}
             >
               <div className="d-flex align-items-center justify-content-between">
-                <Link
-                  to={`/category/${cat.slug}`}
-                  className="text-decoration-none text-light flex-grow-1"
-                >
+                <div className="text-decoration-none text-light flex-grow-1">
                   {cat.name}
-                </Link>
+                </div>
 
                 {hasSub && <FaChevronRight style={{ flexShrink: 0 }} />}
               </div>
